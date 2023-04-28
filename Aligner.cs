2c2cb5335b37d97392313c9d56bb98a1a1d6604a
Elagoht <furkanbaytekin@gmail.com>
2023-04-29 02:03:57 +0300

    feat: aligner class added

---
+++ Aligner.cs
@@ -0,0 +1,30 @@
+namespace Glue
+{
+    public static class Aligner
+    {
+        // Align Left
+        public static string LeftAlign(string text, int width, char filler = ' ')
+        {
+            return text.PadRight(width, filler)
+                .Substring(0, width);
+        }
+        // Align Center
+        public static string CenterAlign(string text, int width, char filler = ' ')
+        {
+            int remaining = width - text.Length;
+            int left = remaining / 2;
+            int right = remaining - left;
+
+            return text
+                .PadLeft(text.Length + left, filler)
+                .PadRight(width, filler);
+        }
+        // Align Right
+        public static string RightAlign(string text, int width, char filler = ' ')
+        {
+            return text
+                .PadLeft(width, filler)
+                .Substring(0, width);
+        }
+    }
+}
