c73124710291e1f99ba8a59adefbbd78e091d68b
Elagoht <furkanbaytekin@gmail.com>
2023-04-30 03:15:26 +0300

    refactor: lesser nester

--- Program.cs
+++ Program.cs
@@ -31,17 +31,29 @@ Valid values :
    To right  : 1 | right",
                     (string value) => {
                         alignment = value == "0" || value.ToLower() =="center"
-                            ? Alignment.Center
-                            : (value == "1" || value.ToLower() == "right"
-                                ? Alignment.Right
-                                : Alignment.Left );
+                            ? Alignment.Center : (value == "1" || value.ToLower() == "right"
+                                ? Alignment.Right : Alignment.Left );
                     }
                 },
                 { "n|noalign", "Do not align fields, overwrites alignment option", (string value)=> { align = value != "no-align" && value !="n"; } },
                 { "d=|delimiter=", "String value that will split the file contents", (string value) => { delimiter = value; } },
                 { "s=|separator=", "String value that will bind the new parts", (string value) => { separator = value; } } ,
                 { "f=|filler=", "Determine what empty areas will be filled with", (string value) => { filler = char.Parse(value.Substring(0,1)); } },
-                { "t|transpose", "Swap columns and rows", (string value) => { transpose = value == "transpose" || value == "t"; } }
+                { "t|transpose", "Swap columns and rows", (string value) => { transpose = value == "transpose" || value == "t"; } },
+                { "csv", "Csv with semicolon, same as -t -n -s \";\"", (string value) => {
+                    if (transpose = value == "csv") {
+                        separator= ";";
+                        align=false;
+                        transpose=true;
+                    }}
+                },
+                { "csv2", "Csv with comma, same as -t -n -s \",\"", (string value) => {
+                    if (transpose = value == "csv2") {
+                        separator= ",";
+                        align=false;
+                        transpose=true;
+                    }}
+                }
             };
 
             // Get unregistered arguments as files
@@ -67,30 +79,20 @@ Valid values :
             }
 
             // Default action
+            if (transpose)
             {
-                if (transpose)
+                if (align)
                 {
-                    if (align)
-                    {
-                        Console.WriteLine(Merger.VerticalAligned(delimiter, separator, alignment, filler, InpFiles(files)));
-                    }
-                    else
-                    {
-                        Console.WriteLine(Merger.Vertical(delimiter, separator, InpFiles(files)));
-                    }
+                    Merger.VerticalAligned(delimiter, separator, alignment, filler, InpFiles(files));
                 }
-                else
-                {
-                    if (align)
-                    {
-                        Console.WriteLine(Merger.HorizontalAligned(delimiter, separator, alignment, filler, InpFiles(files)));
-                    }
-                    else
-                    {
-                        Console.WriteLine(Merger.Horizontal(delimiter, separator, InpFiles(files)));
-                    }
-                }
+                Merger.Vertical(delimiter, separator, InpFiles(files));
             }
+            if (align)
+            {
+                Merger.HorizontalAligned(delimiter, separator, alignment, filler, InpFiles(files));
+            }
+            Merger.Horizontal(delimiter, separator, InpFiles(files));
+
             // Default Exit
             Environment.Exit(0);
         }
